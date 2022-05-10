9989b828efff999785a9f0eb82f1411235cda780
Aman-Mandal <002amankumar@gmail.com>
2022-05-10 14:31:40 +0530

    Init

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict'
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form')
+const containerWorkouts = document.querySelector('.workouts')
+const inputType = document.querySelector('.form__input--type')
+const inputDistance = document.querySelector('.form__input--distance')
+const inputDuration = document.querySelector('.form__input--duration')
+const inputCadence = document.querySelector('.form__input--cadence')
+const inputElevation = document.querySelector('.form__input--elevation')
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const { latitude } = position.coords
+      const { longitude } = position.coords
+      console.log(`https://www.google.com/maps/@${latitude}.${longitude}`)
+
+      const coords = [latitude, longitude]
+      const map = L.map('map').setView(coords, 14)
+      //   console.log(map)
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map)
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng
+        // const lat = mapEvent.latlng.lat
+        // const lng = mapEvent.latlng.lng
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 200,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup()
+      })
+    },
+    () => alert('Unable to detect your location')
+  )
+}
